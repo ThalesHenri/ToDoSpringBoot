0ed581281e5b2c7ab78df781fe1e3e25fd2853d0
ThalesHenri <thales1337@hotmail.com>
2026-09-16 18:53:30 -0300

    [Feature](.gitigaynore) novo gitignore adicionado

---
+++ backend/src/main/java/com/thsistemas/ToDoList/controller/TaskController.java
@@ -0,0 +1,18 @@
+package com.thsistemas.ToDoList.controller;
+
+public class TaskController {
+    //Construtor
+    
+    
+    
+    //Atributos
+
+    
+    
+    
+    //requisições
+        // Get
+        // Post
+        // Put
+        // Delete
+}
